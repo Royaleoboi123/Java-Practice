b267808054c2ae458f9230d8306d7c5d8f26fb2d
Dnyanesh Kale <Dnyanesh Kale@DESKTOP-7O8O5LM>
2024-01-18 18:17:59 +0530

    added first and second class

--- Java practice/src/MainClass.java
+++ Java practice/src/MainClass.java
@@ -3,8 +3,20 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world");
-		
-
+		Second s = new Second();
+		s.func();
 	}
 
+}
+
+class First {
+	public int a;
+	
+}
+
+class Second extends First {
+	void func() {
+		a= 1;
+		System.out.println(a);
+	}
 }
